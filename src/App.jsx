68361fa3c1d6dd8867975245f965df90cb182a21
Filip Fey <filipfey@gmail.com>
2020-05-25 01:24:19 +0200

    clears App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
+/* eslint-disable */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observer, inject } from 'mobx-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default (observer(class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          Veichle app
+        </div>
+      </div>
+    );
+  }
+}))
