f8d42d111eca7511f54392b21e9e87c4fbc196a6
Filip Fey <filipfey@gmail.com>
2020-06-03 16:19:01 +0200

    clears App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,13 @@
 /* eslint-disable */
 import React from 'react';
-import { observer, inject } from 'mobx-react';
 
-export default inject('modelStore', 'makeStore')(observer(class App extends React.Component {
+export default class App extends React.Component {
 
   render() {
-    
-
-    const {setCurrentPage, setFilters, setSortBy, previousPage, nextPage, list: modelList} = this.props.modelStore.pagination;
-    const { list: makeList } = this.props.makeStore.pagination;
-    const {selectModel, selectedModel} = this.props.modelStore;
-    
-
-    const renderSelected = () => {
-      if(!selectedModel) return null;
-
-      return(
-        <div>
-          {selectedModel.id} {selectedModel.name} {selectedModel.make.name}
-        </div>
-      )
-    } 
-
-    const renderUpdate = () => {
-      if(!selectedModel) return null;
-
-      return(
-        <button onClick={() => this.props.modelStore.models[2].updateOnServer({name: 'New name', makeId: 1})}>Change</button>
-      )
-    } 
     return (
       <div className="App">
-        <ul>
-          {modelList.map((m) => <li key={m.id}>{m.id} {m.name} {m.make.name}</li>)}
-        </ul>
-        <button
-           onClick={() => setCurrentPage(previousPage)}
-        >
-            Go to {previousPage}
-        </button>
-        <button
-          onClick={() => setCurrentPage(nextPage)}
-        >Go to {nextPage}</button>
-        <button
-          onClick={() => setFilters([
-            {field: 'make.name', value: 'BMW'}
-          ])}
-        >Filter</button>
-        <button
-          onClick={() => setSortBy(
-            {field: 'name'}
-          )}
-        >Sort</button>
-        <button
-          onClick={() => selectModel(
-            this.props.modelStore.models[2]
-          )}
-        >Select</button>
-        {renderSelected()}
-        {renderUpdate()}
-        <ul>
-          {makeList.map((m) => <li key={m.id}>{m.id} {m.name}</li>)}
-        </ul>
+        My Application
       </div>
     );
   }
-}))
+}
