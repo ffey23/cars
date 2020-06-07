1aabbd7cd5d4acebd5ae22da1dabcf9a8d2287aa
Filip Fey <filipfey@gmail.com>
2020-06-07 02:05:19 +0200

    creates pagination common component

---
+++ src/common/components/Pagination.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Observer, observer } from 'mobx-react';
+import ReactPaginate from 'react-paginate';
+import PaginationClass from '../utils/Pagination';
+
+function Pagination({ pagination }) {
+  const pageCount = Math.ceil(pagination.listCount / pagination.perPage);
+  return (
+    <Observer>
+      { () => (
+        <ReactPaginate
+          pageCount={pageCount}
+          pageRangeDisplayed={10}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => pagination.setCurrentPage(selected + 1)}
+        />
+      )}
+    </Observer>
+  );
+}
+
+Pagination.propTypes = {
+  pagination: PropTypes.instanceOf(PaginationClass).isRequired,
+};
+
+export default observer(Pagination);
