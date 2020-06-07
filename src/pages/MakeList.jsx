b2c559f590665574d151623cfdd55f77439164e9
Filip Fey <filipfey@gmail.com>
2020-06-08 00:47:53 +0200

    creates make list page

--- src/pages/MakeList.jsx
+++ src/pages/MakeList.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { inject, observer } from 'mobx-react';
+import MakeStore from '../stores/make/MakeStore';
+import Pagination from '../components/Pagination';
+import Table from '../components/Table';
+import TableHeader from '../components/Table/TableHeader';
 
-function MakeList() {
+const tableCells = [{ name: 'Make', propertyName: 'name' }];
+function MakeList({ makeStore }) {
   return (
     <div className="make-list">
-      Make List
+      <TableHeader cells={tableCells} pagination={makeStore.pagination} />
+      <Table data={makeStore.pagination.list} cells={tableCells} editLinkBase="make-edit" />
+      <Pagination pagination={makeStore.pagination} />
     </div>
   );
 }
 
 MakeList.propTypes = {
-
+  makeStore: PropTypes.instanceOf(MakeStore).isRequired,
 };
 
-export default MakeList;
+export default inject('makeStore')(observer(MakeList));
