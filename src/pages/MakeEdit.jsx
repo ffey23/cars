ccfce18c6312467a115d9de7066f480fae6ef1cc
Filip Fey <filipfey@gmail.com>
2020-06-09 15:21:35 +0200

    creates make edit component

--- src/pages/MakeEdit.jsx
+++ src/pages/MakeEdit.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useRef } from 'react';
+import {
+  useParams,
+  useHistory,
+} from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import { instanceOf } from 'prop-types';
+import MakeStore from '../stores/make/MakeStore';
+import useLoadMakes from '../common/hooks/useLoadMakes';
 
-function MakeEdit() {
+function updateMake(make, nameInput, history) {
+  make.updateOnServer({
+    name: nameInput.current.value,
+  }).then(() => {
+    history.push('/make-list');
+  });
+}
+function MakeEdit({ makeStore }) {
   const { id } = useParams();
+  useLoadMakes(makeStore);
+  const nameInput = useRef();
+  const history = useHistory();
+
+  if (!makeStore.makes.length) return null;
+  const make = makeStore.makes.find((m) => m.id === Number(id));
   return (
     <div className="make-edit">
-      Make Edit
-      {' '}
-      {id}
+      <div>
+        <label htmlFor={nameInput}>
+          Make name:
+          {' '}
+          <input type="text" defaultValue={make.name} ref={nameInput} />
+        </label>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => updateMake(make, nameInput, history)}
+        >
+          Update
+        </button>
+      </div>
     </div>
   );
 }
 
 MakeEdit.propTypes = {
-
+  makeStore: instanceOf(MakeStore).isRequired,
 };
 
-export default MakeEdit;
+export default inject('makeStore')(observer(MakeEdit));
