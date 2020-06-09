88ffb8b92c3a7d31af66c7619e83b815610c6925
Filip Fey <filipfey@gmail.com>
2020-06-09 15:06:53 +0200

    removes v4() uuid generate because better debuging experience

--- src/common/api/modelApi.js
+++ src/common/api/modelApi.js
@@ -1,5 +1,3 @@
-import { v4 } from 'uuid';
-
 const modelApi = {
   fetchModels() {
     return new Promise((resolve, reject) => {
@@ -7,24 +5,24 @@ const modelApi = {
         const random = Math.random();
         if (random < 1) {
           return resolve([
-            { id: v4(), makeId: 1, name: '325e' },
-            { id: v4(), makeId: 4, name: 'Pickup' },
-            { id: v4(), makeId: 6, name: 'CL550' },
-            { id: v4(), makeId: 3, name: 'Beetle' },
-            { id: v4(), makeId: 2, name: 'Bronco' },
-            { id: v4(), makeId: 5, name: 'Fuego' },
-            { id: v4(), makeId: 2, name: 'Escape' },
-            { id: v4(), makeId: 1, name: '640i' },
-            { id: v4(), makeId: 6, name: '400SE' },
-            { id: v4(), makeId: 3, name: 'Fox' },
-            { id: v4(), makeId: 5, name: '18i' },
-            { id: v4(), makeId: 4, name: 'Rogue' },
-            { id: v4(), makeId: 1, name: '750i' },
-            { id: v4(), makeId: 4, name: 'Quest' },
-            { id: v4(), makeId: 5, name: 'Sportwagon' },
-            { id: v4(), makeId: 2, name: 'Contour' },
-            { id: v4(), makeId: 6, name: '300SL' },
-            { id: v4(), makeId: 3, name: 'Golf' },
+            { id: '1', makeId: 1, name: '325e' },
+            { id: '2', makeId: 4, name: 'Pickup' },
+            { id: '3', makeId: 6, name: 'CL550' },
+            { id: '4', makeId: 3, name: 'Beetle' },
+            { id: '5', makeId: 2, name: 'Bronco' },
+            { id: '6', makeId: 5, name: 'Fuego' },
+            { id: '7', makeId: 2, name: 'Escape' },
+            { id: '8', makeId: 1, name: '640i' },
+            { id: '9', makeId: 6, name: '400SE' },
+            { id: '10', makeId: 3, name: 'Fox' },
+            { id: '11', makeId: 5, name: '18i' },
+            { id: '12', makeId: 4, name: 'Rogue' },
+            { id: '13', makeId: 1, name: '750i' },
+            { id: '14', makeId: 4, name: 'Quest' },
+            { id: '15', makeId: 5, name: 'Sportwagon' },
+            { id: '16', makeId: 2, name: 'Contour' },
+            { id: '17', makeId: 6, name: '300SL' },
+            { id: '18', makeId: 3, name: 'Golf' },
           ]);
         }
         return reject(Error());
