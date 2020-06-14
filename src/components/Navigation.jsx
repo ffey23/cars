f30f52c777507d93b64462dba6353e3ca822812e
Filip Fey <filipfey@gmail.com>
2020-06-14 18:39:13 +0200

    styles navigation module

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Navigation.module.scss';
 
 export default function Navigation() {
   return (
-    <nav>
-      <Link to="/model-list">Models</Link>
-      <Link to="/make-list">Makes</Link>
+    <nav className={styles.navigation}>
+      <Link className={styles.link} to="/model-list">Models</Link>
+      <Link className={styles.link} to="/make-list">Makes</Link>
     </nav>
   );
 }
