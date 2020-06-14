aa93bd0526623f9d15c10ead8118a9372aa6d587
Filip Fey <filipfey@gmail.com>
2020-06-14 22:18:36 +0200

    styles pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Observer, observer } from 'mobx-react';
 import ReactPaginate from 'react-paginate';
 import PaginationClass from '../common/utils/Pagination';
+import styles from './Pagination.module.scss';
 
 function Pagination({ pagination }) {
   const pageCount = Math.ceil(pagination.listCount / pagination.perPage);
@@ -14,6 +15,15 @@ function Pagination({ pagination }) {
           pageRangeDisplayed={10}
           marginPagesDisplayed={1}
           onPageChange={({ selected }) => pagination.setCurrentPage(selected + 1)}
+          containerClassName={styles.pagination}
+          pageClassName={styles.page}
+          previousClassName={styles.previous}
+          nextClassName={styles.next}
+          activeClassName={styles.pageActive}
+          pageLinkClassName={styles.pageLink}
+          nextLinkClassName={styles.nextLink}
+          previousLinkClassName={styles.previousLink}
+          disabledClassName={styles.disabled}
         />
       )}
     </Observer>
